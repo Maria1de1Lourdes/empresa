35b3e3d3a08ba9eb9a06c93f9156cdb714d735dc
Maria1de1Lourdes <lourdes.freitas@academico.ifpb.edu.br>
2026-04-29 11:17:58 -0300

    Update empresa.py

--- empresa.py
+++ empresa.py
@@ -1,31 +1,11 @@
-import streamlit as st
-st.image(
-    "gucci.png",
-    caption="Acesse a Página da GUCCI por aqui",
-    link="https://www.gucci.com/int/en/st/brazil-landing",
-)
+st.image("gucci.png")
 st.write("Maria de Lourdes de Freitas Almeida")
 st.image(
-        "Foto.jpg"
+        "Foto.jpg",
+         width=320
         )
 st.write("Tenho mestrado em Design e em administração, fui contratada pela Gucci após uma entrevista acirrada. Atualmente faço parte na modelagem dos sites.")
 
-st.image(
-    "whatsapp.png",
-    caption="Acesse o whattsapp oficial da GUCCI por aqui",
-    link="https://w.app/gucci"
-)
-st.image(
-    "QRcode.png"
-)
-st.markdown(
-    """
-    <style>
-    .stApp {
-        background-color: #2E3135;
-        text-color: #FFFFFF;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
+st.image("whatsapp.png")
+st.image("QRcode.png")
+st.button("acesse aqui o whatsapp","link="https://w.app/gucci")
