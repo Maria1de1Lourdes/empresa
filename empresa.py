73d225c65017e8753976b9c2dc0fbdd814b910ef
Maria1de1Lourdes <lourdes.freitas@academico.ifpb.edu.br>
2026-05-11 11:54:57 -0300

    Update empresa.py

--- empresa.py
+++ empresa.py
@@ -1,3 +1,78 @@
+import streamlit as st
+
+import base64
+st.set_page_config(page_title="Perfil da Gucci", layout="wide")
+def get_base64_image(path):
+    with open(path, "rb") as img_file:
+        return base64.b64encode(img_file.read()).decode()
+img_base64 = get_base64_image("gucci.png")
+zap_base64 = get_base64_image("whatsapp.png")
+ 
+col1, col2, col3 = st.columns([1,2,1])
+with col2:
+    st.markdown(f"""
+        <div style="text-align: center; margin-bottom: 50px;">
+            <a href="https://starlink.com/" target="_blank">
+                <img src="data:image/png;base64,{img_base64}"
+                     width="320"
+                     style="border-radius:12px;">
+            </a>
+        </div>
+    """, unsafe_allow_html=True)
+col_left, col_right = st.columns([3,1])
+with col_left:
+    st.markdown("""
+    <div style='margin-bottom:30px; font-size:30px;'>
+        <b>Nome: Maria de Lourdes de Freitas Almeida </b>
+    </div>
+    """, unsafe_allow_html=True)
+    subcol1, subcol2 = st.columns([1,4])
+    with subcol1:
+        st.markdown("""
+        <div style="
+            display: flex;
+            align-items: center;
+            height: 100%;
+        ">
+        """, unsafe_allow_html=True)
+ 
+        st.image("Foto.jpg", width=800)
+ 
+        st.markdown("</div>", unsafe_allow_html=True)
+ 
+    # TEXTO
+    with subcol2:
+        st.markdown("""
+        <div style="
+            text-align: justify;
+            font-size: 20px;
+            line-height: 2.0;
+            width: 100%;
+            max-width: none;
+        ">
+            <b>Sobre Maria:<br>
+           Maria de Lourdes de Freitas Almeida é uma  participante do curso profissionalizante de informática, sendo uma das pioneiras desse curso no instituto IFPB de Itabaiana.
+        </div>
+        """, unsafe_allow_html=True)
+ 
+    st.markdown("<div style='margin-top:30px;'>", unsafe_allow_html=True)
+    st.link_button("Acessar", )
+    st.markdown("</div>", unsafe_allow_html=True)
+ 
+with col_right:
+    st.empty()
+ 
+# �� NOVO BLOCO (WhatsApp clicável no final)
+st.markdown(f"""
+    <div style="text-align: center; margin-top: 10px;">
+        <a href="https://wa.me/5583998234415" target="_blank">
+            <img src="data:image/png;base64,{zap_base64}" width="100">
+        </a>
+    </div>
+""", unsafe_allow_html=True)
+
+
+
 st.image("gucci.png")
 st.write("Maria de Lourdes de Freitas Almeida")
 st.image(
