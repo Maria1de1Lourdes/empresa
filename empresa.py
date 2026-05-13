8f9888704955dbd7ffceecab6aeef39a58db0531
Maria1de1Lourdes <lourdes.freitas@academico.ifpb.edu.br>
2026-05-13 11:08:26 -0300

    Update empresa.py1

--- empresa.py
+++ empresa.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 import base64
-st.set_page_config(page_title="Perfil da Gucci", layout="wide")
+st.set_page_config(page_title="Perfil da Gucci", layout="wide",background_color="black")
 def get_base64_image(path):
     with open(path, "rb") as img_file:
         return base64.b64encode(img_file.read()).decode()
